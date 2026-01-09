a0419b60fdb724a4b07dc3842cf4b9b6bcd53863
Aqleyca <azrililfadli@gmail.com>
2026-01-09 14:35:51 +0545

    second commit

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -1,13 +1,12 @@
-// lib/env/env.dart
 import 'package:envied/envied.dart';
 
 part 'env.g.dart';
 
 @Envied(path: '.env')
 abstract class Env {
-    @EnviedField(varName: 'CLOUDINARY_CLOUD_NAME', obfuscate: true)
+    @EnviedField(varName: 'CLAUDYNARY_CLOUD_NAME', obfuscate: true)
     static final String cloudName = _Env.cloudName;
     
-    @EnviedField(varName: 'CLOUDINARY_UPLOAD_PRESET', obfuscate: true)
+    @EnviedField(varName: 'CLAUDYNARY_UPLOAD_PRESET', obfuscate: true)
     static final String uploadPreset = _Env.uploadPreset;
 }
